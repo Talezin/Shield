3e8998a5b6e6438a0d993b845f98f4746383e991
Thallis <104874259+Talezin@users.noreply.github.com>
2023-01-29 21:55:14 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const botaoMenu = document.querySelector('.cabecalho__menu')
-const menu = document.querySelector('.nav-ul')
-
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('nav-ul--ativo')
-})
